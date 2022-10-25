50e5292544115f6af2f918ce796f4acda81c7346
Catarina Skibsrud <catarins@stud.ntnu.no>
2022-10-25 12:06:31 +0200

    added reviews to frontend

--- Frontend/src/components/SongDetails.tsx
+++ Frontend/src/components/SongDetails.tsx
@@ -1,6 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
 import { useEffect, useState } from 'react';
 import { SongAllData } from '../helpers/types';
+import ReviewComponent from './ReviewComponent';
 
 const GET_SONG_BY_SONGID = gql`
     query GetSong ($songID: Int!) {
@@ -16,16 +17,16 @@ const GET_SONG_BY_SONGID = gql`
     }
 `;
 
-const SongDetails= ({songID}: {songID: number}) => {
+const SongDetails = ({ songID }: { songID: number }) => {
 
     const [song, setSong] = useState<SongAllData>();
     const { loading, error, data } = useQuery(GET_SONG_BY_SONGID, { variables: { songID: songID } });
-    
+
     useEffect(() => {
         if (data) {
             setSong(data.songBySongID[0])
         }
-    },[data])
+    }, [data])
 
     if (loading) return <h1>Loading...</h1>
     if (error) return <h1>`Error! ${error.message}`</h1>;
@@ -33,14 +34,17 @@ const SongDetails= ({songID}: {songID: number}) => {
 
     return (
         <div className='songDetails'>
-            <img className='imgDetails' src={song.imageURL}></img>
-            <div className='textbox'>
-                <h1>{song?.songName} </h1>
-                <p>Artist: {song?.artistName}</p>
-                <p>Year: {song?.year}</p>
-                <p>Duration: {song?.durationMS}</p>
-                <p>Energy: {song?.energy}</p>
+            <div className="song-info">
+                <img className='imgDetails' src={song.imageURL}></img>
+                <div className='textbox'>
+                    <h1>{song?.songName} </h1>
+                    <p>Artist: {song?.artistName}</p>
+                    <p>Year: {song?.year}</p>
+                    <p>Duration: {song?.durationMS}</p>
+                    <p>Energy: {song?.energy}</p>
+                </div>
             </div>
+            <ReviewComponent songID={songID} />
         </div>
     )
 }
