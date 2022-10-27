ae115ebc6ba510e27171ff8bb580c375d7951d61
erik2501 <eriksw2501@gmail.com>
2022-10-27 13:52:02 +0200

    Styling changes to song-details

--- Frontend/src/components/SongDetails.tsx
+++ Frontend/src/components/SongDetails.tsx
@@ -4,6 +4,13 @@ import { SongAllData } from '../helpers/types';
 import ListReviews from './ListReviewsComponent';
 import ReviewCard from './ReviewCard';
 import ReviewComponent from './ReviewComponent';
+import { Card, CardContent, Box, Typography, CardMedia } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import TimerIcon from '@mui/icons-material/Timer';
+
 
 const GET_SONG_BY_SONGID = gql`
     query GetSong ($songID: Int!) {
@@ -35,6 +42,7 @@ const SongDetails = ({ songID }: { songID: number }) => {
     if (!song) return <h1>Could not find this song.</h1>
 
     return (
+        /*
         <div>
             <div className='songDetails'>
                 <div className="song-info">
@@ -53,7 +61,44 @@ const SongDetails = ({ songID }: { songID: number }) => {
                 <ListReviews songID={songID} />
             </div>
         </div>
+        */
 
+        <div className='songDetails'>
+     <Card sx={{ display: 'flex', marginRight:15}}>
+          <CardMedia
+                component="img"
+                sx={{height: 300 }}
+                image={song.imageURL}
+                alt="Live from space album cover"
+            />
+            <Box>
+                <Box sx={{display : 'flex', width : '200px',marginTop:'12%'}}>
+                    <MusicNoteIcon></MusicNoteIcon>
+                    <Typography>{song?.songName}</Typography>
+                </Box>
+                <Box sx={{display : 'flex', width : '200px',marginTop:'12%'}}>
+                    <PersonIcon></PersonIcon>
+                    <Typography>{song?.artistName}</Typography>
+                </Box>
+                <Box sx={{display : 'flex', width : '200px',marginTop:'12%'}}>
+                    <CalendarMonthIcon></CalendarMonthIcon>
+                    <Typography>{song?.year}</Typography>
+                </Box>
+                <Box sx={{display : 'flex', width : '200px',marginTop:'12%'}}>
+                    <TimerIcon></TimerIcon>
+                    <Typography>{Math.floor(song?.durationMS / (1000*60))}:{Math.round((song?.durationMS/1000) % (60))}</Typography>
+                </Box>
+                <Box sx={{display : 'flex', width : '200px', marginTop:'12%'}}>
+                    <ElectricBoltIcon></ElectricBoltIcon>
+                    <Typography>{song?.energy}</Typography>
+                </Box>
+            </Box>
+     </Card>
+     <ReviewComponent songID={songID} />
+    <div className="reviewList">
+        <ListReviews songID={songID} />
+    </div>
+    </div>
 
     )
 }
