7bcdb0b79cd5a4f554eaf81ec5331824ff6f9018
pudderludde <ludvikb@online.no>
2022-10-27 10:48:02 +0200

    Made pagination with material UI

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -3,8 +3,10 @@ import Searchbar from '../components/Searchbar';
 import SongTable from '../components/SongTable';
 import { gql, useLazyQuery } from '@apollo/client';
 import { debounce } from '../helpers/utils';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
-const PAGE_SIZE = 20;
+const PAGE_SIZE = 10;
 
 const GET_COUNT = gql`
     query ( $searchWord: String ){
@@ -26,7 +28,6 @@ const HomePage = () => {
         sessionStorage.setItem('searchWord', searchWord);
     },[searchWord])
 
-
     useEffect(() => {
         sessionStorage.setItem('offset', offset.toString());
     },[offset])
@@ -36,23 +37,20 @@ const HomePage = () => {
         
     }, [searchWord])
 
-
-    let pageNumber = offset/PAGE_SIZE + 1
     const maxPages = Math.ceil((data?.songSearchCount ?? 1) / PAGE_SIZE);
+
+    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setOffset((value-1)*PAGE_SIZE)
+    }
 
     return (
         <div className='flexColCenterCenter'>
 
             <Searchbar setOffset={setOffset} searchWord={searchWord} setSearchWord={setSearchWord}/>
             <SongTable PAGE_SIZE={PAGE_SIZE} offset={offset} searchWord={searchWord} />
-
-            <div className='page-manager'>
-                <button style={{ visibility: pageNumber-2 <= 0 ? 'hidden' : 'visible'}}  onClick={() => setOffset(offset-2*PAGE_SIZE)}>{pageNumber-2} </button>
-                <button style={{ visibility: pageNumber-1 <= 0 ? 'hidden' : 'visible'}} onClick={() => setOffset(offset-PAGE_SIZE)}>{pageNumber-1} </button>
-                <button style={{ backgroundColor: 'gray', opacity: '0.7', cursor: 'default'}}>{pageNumber} </button>
-                <button style={{ visibility: pageNumber+1 > maxPages ? 'hidden' : 'visible'}} onClick={() => setOffset(offset+PAGE_SIZE)}>{pageNumber+1} </button>
-                <button style={{ visibility: pageNumber+2 > maxPages ? 'hidden' : 'visible'}} onClick={() => setOffset(offset+2*PAGE_SIZE)}>{pageNumber+2} </button>
-            </div>
+            <Stack spacing={2}>
+                <Pagination count={maxPages} page={offset/PAGE_SIZE + 1} onChange={handleChange} variant="outlined" color="primary" />
+            </Stack>
         </div>
     )
 }
